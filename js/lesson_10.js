f6b923947a52358f92cfa94196a4a4c729734d4b
Oleg Marics <marich763@gmail.com>
2024-07-04 21:20:19 +0200

    04.07

--- js/lesson_10.js
+++ js/lesson_10.js
@@ -3,7 +3,7 @@ const questions = [
 		id: 5.1_1,
 		question: "",
 		checkFunction: function () {
-			//document.write(`<div>${}</div><div><a href="../components/lesson9.html">Повернутися до уроку</a></div>`)
+			//document.write(`<div>${}</div><div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
 		}
 	},
 	// ========================================================================================================================================
@@ -11,18 +11,85 @@ const questions = [
 		id: 5.2_1,
 		question: "",
 		checkFunction: function () {
-			//document.write(`<div>${}</div><div><a href="../components/lesson9.html">Повернутися до уроку</a></div>`)
+			//document.write(`<div>${}</div><div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
 		}
 	},
 	// ========================================================================================================================================
 	{
-		id: 5.3_2,
-		question: "",
+		id: 5.3_1,
+		question: "Дано масив цін. вивести їх окремими div",
 		checkFunction: function () {
-			//document.write(`<div>${}</div><div><a href="../components/lesson9.html">Повернутися до уроку</a></div>`)
+			let prices = [100, 90, 5, 13, 17, 16, 18]
+			prices.forEach(
+				(element, index) => document.write(`<div>${index + 1} - ${element} грн.</div>`)
+			)
+			document.write(`<div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
 		}
 	},
+	{
+		id: 5.3_2,
+		question: "Задано масив імен праццівників. Сформувати масив. який складається з перших літкр імен",
+		checkFunction: function () {
+			let names = [
+				"Марія", "Павло", "Анна", "Федір", "Григорій", "Юлія", "Зеновій", "Сергій", "Катерина", "Євгенія", "Дарина", "Богдан", "Ірина", "Тетяна", "Олена", "Назар", "Леонід", "Роксолана", "Уляна", "Христина", "Цезар", "Черемис", "Шарлотта", "Щастя", "Йосип", "Жанна", "Ярослав"
+			];
+			let firstLettersArray = names.map(name => name[0])
+			document.write(`<div>${firstLettersArray.join(', ')}</div><div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
+			//document.write(`<div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
+		}
 
+	},
+	{
+		id: 5.3_3,
+		question: "Дано масив днів. Сформувати масив з відповідними назвами днів",
+		checkFunction: function () {
+			let daysNames = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота", "Неділя"];
+			let daysNumbersList = [3, 2, 1, 5, 4, 7]
+			let daysNamesList = daysNumbersList.map(
+				(dayNumber) => daysNames[dayNumber - 1]
+			)
+			document.write(`<div>${daysNamesList.join(', ')}</div><div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
+		}
+	},
+	{
+		id: 5.3_4,
+		question: "Реєструвати імена користувачів (вводити і зберігати в масиві).",
+		checkFunction: function () {
+			let users = []
+			for (let i = 0; i < 3; i++) {
+				let name = prompt('User name')
+				users.push(name)
+			}
+			while (users.length > 0) {
+				let name = users.pop()
+				document.write(`<div>${name}</div>`)
+			}
+			document.write(`<div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
+		}
+	},
+	{
+		id: 5.3_5,
+		question: "Дано упорядкований за зростанням масив цін. Сформувати новий масив, Сформувати новий масив, який складається тільки з різних значень",
+		checkFunction: function () {
+			let prices = [67, 89, 89, 89, 89, 89, 90, 90, 122, 122, 122, 900]
+
+			let uniquePrices = []
+
+			for (let i = 0; i < prices.length; i++) {
+				if (i === 0 || prices[i] !== prices[i - 1])
+					uniquePrices.push(prices[i])
+			}
+			//document.write(`<div>${}</div>`)
+			document.write(`<div>${uniquePrices}</div><div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
+		}
+	},
+	// {
+	// 	id: 5.2_1,
+	// 	question: "",
+	// 	checkFunction: function () {
+	// 		//document.write(`<div>${}</div><div><a href="../components/lesson10.html">Повернутися до уроку</a></div>`)
+	// 	}
+	// },
 ];
 
 function showQuestion(questionId) {
